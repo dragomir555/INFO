621dd93d7a21c32022388ebdd670700d4f718e00
Dejan Mijic <mijicdejan95@gmail.com>
2018-12-14 23:57:29 +0100

    Dodana forma za promjenu lozinke.

--- Autopraonica/Autopraonica_Markus/forms/LoginForm.Designer.cs
+++ Autopraonica/Autopraonica_Markus/forms/LoginForm.Designer.cs
@@ -28,9 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             this.lblUsername = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
             this.lblPassword = new System.Windows.Forms.Label();
             this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -40,47 +41,47 @@
             this.lblUsername.AutoSize = true;
             this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUsername.ForeColor = System.Drawing.Color.White;
-            this.lblUsername.Location = new System.Drawing.Point(162, 37);
+            this.lblUsername.Location = new System.Drawing.Point(174, 40);
             this.lblUsername.Name = "lblUsername";
             this.lblUsername.Size = new System.Drawing.Size(152, 25);
             this.lblUsername.TabIndex = 0;
             this.lblUsername.Text = "Korisničko ime";
             // 
-            // textBox1
+            // tbUsername
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(157, 65);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(167, 29);
-            this.textBox1.TabIndex = 1;
+            this.tbUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUsername.Location = new System.Drawing.Point(165, 70);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(170, 29);
+            this.tbUsername.TabIndex = 1;
             // 
-            // textBox2
+            // tbPassword
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(157, 135);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(167, 29);
-            this.textBox2.TabIndex = 2;
-            this.textBox2.UseSystemPasswordChar = true;
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(165, 135);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(170, 29);
+            this.tbPassword.TabIndex = 2;
+            this.tbPassword.UseSystemPasswordChar = true;
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
             this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPassword.ForeColor = System.Drawing.Color.White;
-            this.lblPassword.Location = new System.Drawing.Point(195, 107);
+            this.lblPassword.Location = new System.Drawing.Point(206, 105);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(87, 25);
-            this.lblPassword.TabIndex = 4;
+            this.lblPassword.TabIndex = 0;
             this.lblPassword.Text = "Lozinka";
             // 
             // btnLogin
             // 
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.Location = new System.Drawing.Point(185, 181);
+            this.btnLogin.Location = new System.Drawing.Point(195, 175);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(110, 34);
-            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Size = new System.Drawing.Size(110, 35);
+            this.btnLogin.TabIndex = 3;
             this.btnLogin.Text = "Prijavi me";
             this.btnLogin.UseVisualStyleBackColor = true;
             // 
@@ -89,14 +90,17 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(65)))), ((int)(((byte)(150)))));
-            this.ClientSize = new System.Drawing.Size(494, 280);
+            this.ClientSize = new System.Drawing.Size(484, 252);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.lblPassword);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
             this.Controls.Add(this.lblUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "LoginForm";
-            this.Text = "LoginForm";
+            this.Text = "Markus";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,8 +109,8 @@
         #endregion
 
         private System.Windows.Forms.Label lblUsername;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
         private System.Windows.Forms.Label lblPassword;
         private System.Windows.Forms.Button btnLogin;
     }
---
+++ Autopraonica/Autopraonica_Markus/forms/PasswordChangeForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Autopraonica_Markus.forms
+{
+    partial class PasswordChangeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PasswordChangeForm));
+            this.lblCurrentPassword = new System.Windows.Forms.Label();
+            this.lblNewPassword = new System.Windows.Forms.Label();
+            this.tbCurrentPassword = new System.Windows.Forms.TextBox();
+            this.tbNewPassword = new System.Windows.Forms.TextBox();
+            this.lblNewPasswordConfirm = new System.Windows.Forms.Label();
+            this.tbNewPasswordConfirm = new System.Windows.Forms.TextBox();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCurrentPassword
+            // 
+            this.lblCurrentPassword.AutoSize = true;
+            this.lblCurrentPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentPassword.ForeColor = System.Drawing.Color.White;
+            this.lblCurrentPassword.Location = new System.Drawing.Point(164, 40);
+            this.lblCurrentPassword.Name = "lblCurrentPassword";
+            this.lblCurrentPassword.Size = new System.Drawing.Size(172, 25);
+            this.lblCurrentPassword.TabIndex = 0;
+            this.lblCurrentPassword.Text = "Trenutna lozinka";
+            // 
+            // lblNewPassword
+            // 
+            this.lblNewPassword.AutoSize = true;
+            this.lblNewPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNewPassword.ForeColor = System.Drawing.Color.White;
+            this.lblNewPassword.Location = new System.Drawing.Point(182, 105);
+            this.lblNewPassword.Name = "lblNewPassword";
+            this.lblNewPassword.Size = new System.Drawing.Size(136, 25);
+            this.lblNewPassword.TabIndex = 0;
+            this.lblNewPassword.Text = "Nova lozinka";
+            // 
+            // tbCurrentPassword
+            // 
+            this.tbCurrentPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCurrentPassword.Location = new System.Drawing.Point(165, 70);
+            this.tbCurrentPassword.Name = "tbCurrentPassword";
+            this.tbCurrentPassword.Size = new System.Drawing.Size(170, 29);
+            this.tbCurrentPassword.TabIndex = 1;
+            this.tbCurrentPassword.UseSystemPasswordChar = true;
+            // 
+            // tbNewPassword
+            // 
+            this.tbNewPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNewPassword.Location = new System.Drawing.Point(165, 135);
+            this.tbNewPassword.Name = "tbNewPassword";
+            this.tbNewPassword.Size = new System.Drawing.Size(170, 29);
+            this.tbNewPassword.TabIndex = 2;
+            this.tbNewPassword.UseSystemPasswordChar = true;
+            // 
+            // lblNewPasswordConfirm
+            // 
+            this.lblNewPasswordConfirm.AutoSize = true;
+            this.lblNewPasswordConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNewPasswordConfirm.ForeColor = System.Drawing.Color.White;
+            this.lblNewPasswordConfirm.Location = new System.Drawing.Point(143, 170);
+            this.lblNewPasswordConfirm.Name = "lblNewPasswordConfirm";
+            this.lblNewPasswordConfirm.Size = new System.Drawing.Size(213, 25);
+            this.lblNewPasswordConfirm.TabIndex = 0;
+            this.lblNewPasswordConfirm.Text = "Potvrda nove lozinke";
+            // 
+            // tbNewPasswordConfirm
+            // 
+            this.tbNewPasswordConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNewPasswordConfirm.Location = new System.Drawing.Point(165, 200);
+            this.tbNewPasswordConfirm.Name = "tbNewPasswordConfirm";
+            this.tbNewPasswordConfirm.Size = new System.Drawing.Size(170, 29);
+            this.tbNewPasswordConfirm.TabIndex = 3;
+            this.tbNewPasswordConfirm.UseSystemPasswordChar = true;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirm.Location = new System.Drawing.Point(195, 240);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(110, 35);
+            this.btnConfirm.TabIndex = 4;
+            this.btnConfirm.Text = "Potvrdi";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            // 
+            // PasswordChangeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(65)))), ((int)(((byte)(150)))));
+            this.ClientSize = new System.Drawing.Size(484, 317);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.tbNewPasswordConfirm);
+            this.Controls.Add(this.lblNewPasswordConfirm);
+            this.Controls.Add(this.tbNewPassword);
+            this.Controls.Add(this.tbCurrentPassword);
+            this.Controls.Add(this.lblNewPassword);
+            this.Controls.Add(this.lblCurrentPassword);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "PasswordChangeForm";
+            this.Text = "Markus";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCurrentPassword;
+        private System.Windows.Forms.Label lblNewPassword;
+        private System.Windows.Forms.TextBox tbCurrentPassword;
+        private System.Windows.Forms.TextBox tbNewPassword;
+        private System.Windows.Forms.Label lblNewPasswordConfirm;
+        private System.Windows.Forms.TextBox tbNewPasswordConfirm;
+        private System.Windows.Forms.Button btnConfirm;
+    }
+}
