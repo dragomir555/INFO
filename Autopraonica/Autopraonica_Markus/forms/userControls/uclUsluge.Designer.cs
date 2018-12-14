669770f1442269033512f611f6fcf18c28ba4bae
Dejan Mijic <mijicdejan95@gmail.com>
2018-12-14 19:16:53 +0100

    Izmjene u uclUsluge

--- Autopraonica/Autopraonica_Markus/forms/userControls/uclUsluge.Designer.cs
+++ Autopraonica/Autopraonica_Markus/forms/userControls/uclUsluge.Designer.cs
@@ -28,24 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvLegalEntity = new System.Windows.Forms.DataGridView();
             this.rbPravnaLica = new System.Windows.Forms.RadioButton();
             this.rbFizickaLica = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Naziv = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.JIB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Grad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Prezime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RegistarskeTablice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLegalEntity)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvLegalEntity
             // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.dgvLegalEntity.AllowUserToAddRows = false;
+            this.dgvLegalEntity.AllowUserToDeleteRows = false;
+            this.dgvLegalEntity.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 205);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(601, 177);
-            this.dataGridView1.TabIndex = 0;
+            this.dgvLegalEntity.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLegalEntity.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Naziv,
+            this.JIB,
+            this.Grad,
+            this.Ime,
+            this.Prezime,
+            this.RegistarskeTablice});
+            this.dgvLegalEntity.Location = new System.Drawing.Point(0, 205);
+            this.dgvLegalEntity.Name = "dgvLegalEntity";
+            this.dgvLegalEntity.ReadOnly = true;
+            this.dgvLegalEntity.Size = new System.Drawing.Size(601, 177);
+            this.dgvLegalEntity.TabIndex = 0;
+            this.dgvLegalEntity.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLegalEntity_CellContentClick);
             // 
             // rbPravnaLica
             // 
@@ -91,6 +108,42 @@
             this.button2.Text = "Dodavanje usluge za fizička lica";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // Naziv
+            // 
+            this.Naziv.HeaderText = "Naziv";
+            this.Naziv.Name = "Naziv";
+            this.Naziv.ReadOnly = true;
+            // 
+            // JIB
+            // 
+            this.JIB.HeaderText = "JIB";
+            this.JIB.Name = "JIB";
+            this.JIB.ReadOnly = true;
+            // 
+            // Grad
+            // 
+            this.Grad.HeaderText = "Grad";
+            this.Grad.Name = "Grad";
+            this.Grad.ReadOnly = true;
+            // 
+            // Ime
+            // 
+            this.Ime.HeaderText = "Ime";
+            this.Ime.Name = "Ime";
+            this.Ime.ReadOnly = true;
+            // 
+            // Prezime
+            // 
+            this.Prezime.HeaderText = "Prezime";
+            this.Prezime.Name = "Prezime";
+            this.Prezime.ReadOnly = true;
+            // 
+            // RegistarskeTablice
+            // 
+            this.RegistarskeTablice.HeaderText = "Registarske tablice";
+            this.RegistarskeTablice.Name = "RegistarskeTablice";
+            this.RegistarskeTablice.ReadOnly = true;
+            // 
             // uclUsluge
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -100,10 +153,10 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.rbFizickaLica);
             this.Controls.Add(this.rbPravnaLica);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvLegalEntity);
             this.Name = "uclUsluge";
             this.Size = new System.Drawing.Size(601, 382);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLegalEntity)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,10 +164,16 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvLegalEntity;
         private System.Windows.Forms.RadioButton rbPravnaLica;
         private System.Windows.Forms.RadioButton rbFizickaLica;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Naziv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn JIB;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Grad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Prezime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RegistarskeTablice;
     }
 }
