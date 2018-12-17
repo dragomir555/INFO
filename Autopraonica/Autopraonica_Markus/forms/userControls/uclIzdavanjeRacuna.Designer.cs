34e46465ef24b07ff7075066acef01d63da15fbe
Nikola Vidovic <nikolavidovic47@yahoo.com>
2018-12-17 17:04:59 +0100

    Dodana forma za izdavanje racuna (bez funkcionalnosti)

--- Autopraonica/Autopraonica_Markus/forms/userControls/uclIzdavanjeRacuna.Designer.cs
+++ Autopraonica/Autopraonica_Markus/forms/userControls/uclIzdavanjeRacuna.Designer.cs
@@ -28,18 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("");
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGenBill = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.hdSerNumSer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hdTypeSer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hdDateSer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hdPriceSer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Naziv klijenta";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(155, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 26);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.Text = "    Odabir klijenta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Datum do";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Datum od";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(155, 147);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 8;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(155, 101);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(328, 419);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 38);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "00.0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(52, 419);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(270, 38);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Ukupna suma   :";
+            // 
+            // btnGenBill
+            // 
+            this.btnGenBill.Location = new System.Drawing.Point(644, 373);
+            this.btnGenBill.Name = "btnGenBill";
+            this.btnGenBill.Size = new System.Drawing.Size(154, 34);
+            this.btnGenBill.TabIndex = 14;
+            this.btnGenBill.Text = "Generiši račun";
+            this.btnGenBill.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.hdSerNumSer,
+            this.hdTypeSer,
+            this.hdDateSer,
+            this.hdPriceSer});
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem2});
+            this.listView1.Location = new System.Drawing.Point(59, 197);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(557, 210);
+            this.listView1.TabIndex = 13;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // hdSerNumSer
+            // 
+            this.hdSerNumSer.Text = "Redni broj usluge";
+            this.hdSerNumSer.Width = 137;
+            // 
+            // hdTypeSer
+            // 
+            this.hdTypeSer.Text = "Vrsta usluge";
+            this.hdTypeSer.Width = 137;
+            // 
+            // hdDateSer
+            // 
+            this.hdDateSer.Text = "Datum usluge";
+            this.hdDateSer.Width = 137;
+            // 
+            // hdPriceSer
+            // 
+            this.hdPriceSer.Text = "Cijena usluge";
+            this.hdPriceSer.Width = 137;
             // 
             // uclIzdavanjeRacuna
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnGenBill);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "uclIzdavanjeRacuna";
-            this.Size = new System.Drawing.Size(601, 382);
+            this.Size = new System.Drawing.Size(801, 470);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnGenBill;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader hdSerNumSer;
+        private System.Windows.Forms.ColumnHeader hdTypeSer;
+        private System.Windows.Forms.ColumnHeader hdDateSer;
+        private System.Windows.Forms.ColumnHeader hdPriceSer;
     }
 }
