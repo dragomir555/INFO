eb056eb59f5dab217f83bc823e7233b5906f493d
Nikola Vidovic <nikolavidovic47@yahoo.com>
2018-12-18 18:57:09 +0100

    Generisanje PDF-a i uredjene kolone u listView

--- Autopraonica/Autopraonica_Markus/forms/userControls/uclIzdavanjeRacuna.Designer.cs
+++ Autopraonica/Autopraonica_Markus/forms/userControls/uclIzdavanjeRacuna.Designer.cs
@@ -35,25 +35,25 @@
             this.label1 = new System.Windows.Forms.Label();
             this.dtpDateTo = new System.Windows.Forms.DateTimePicker();
             this.dtpDateFrom = new System.Windows.Forms.DateTimePicker();
-            this.label5 = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.btnDspUnpSer = new System.Windows.Forms.Button();
             this.lvUpSer = new System.Windows.Forms.ListView();
             this.hdSerNumSer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.hdTypeSer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.hdDateSer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.hdPriceSer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hdFirstName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hdSecName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hdDtTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hdLicPl = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hdPrice = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.btnGenBill = new System.Windows.Forms.Button();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hdSubtypeSer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(61, 66);
+            this.label3.Location = new System.Drawing.Point(16, 61);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(91, 17);
             this.label3.TabIndex = 12;
@@ -63,16 +63,16 @@
             // 
             this.cmbClients.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbClients.FormattingEnabled = true;
-            this.cmbClients.Location = new System.Drawing.Point(155, 61);
+            this.cmbClients.Location = new System.Drawing.Point(122, 56);
             this.cmbClients.Name = "cmbClients";
-            this.cmbClients.Size = new System.Drawing.Size(200, 26);
+            this.cmbClients.Size = new System.Drawing.Size(247, 26);
             this.cmbClients.TabIndex = 11;
             this.cmbClients.Text = "    Odabir klijenta";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(61, 147);
+            this.label2.Location = new System.Drawing.Point(16, 147);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 17);
             this.label2.TabIndex = 10;
@@ -81,7 +81,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(61, 101);
+            this.label1.Location = new System.Drawing.Point(16, 101);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(69, 17);
             this.label1.TabIndex = 9;
@@ -89,27 +89,27 @@
             // 
             // dtpDateTo
             // 
-            this.dtpDateTo.Location = new System.Drawing.Point(155, 147);
+            this.dtpDateTo.Location = new System.Drawing.Point(122, 142);
             this.dtpDateTo.Name = "dtpDateTo";
-            this.dtpDateTo.Size = new System.Drawing.Size(200, 22);
+            this.dtpDateTo.Size = new System.Drawing.Size(247, 22);
             this.dtpDateTo.TabIndex = 8;
             // 
             // dtpDateFrom
             // 
-            this.dtpDateFrom.Location = new System.Drawing.Point(155, 101);
+            this.dtpDateFrom.Location = new System.Drawing.Point(122, 96);
             this.dtpDateFrom.Name = "dtpDateFrom";
-            this.dtpDateFrom.Size = new System.Drawing.Size(200, 22);
+            this.dtpDateFrom.Size = new System.Drawing.Size(247, 22);
             this.dtpDateFrom.TabIndex = 7;
             // 
-            // label5
+            // lblPrice
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(328, 419);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(84, 38);
-            this.label5.TabIndex = 16;
-            this.label5.Text = "00.0";
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(328, 419);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(84, 38);
+            this.lblPrice.TabIndex = 16;
+            this.lblPrice.Text = "00.0";
             // 
             // label4
             // 
@@ -117,13 +117,13 @@
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(52, 419);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(270, 38);
+            this.label4.Size = new System.Drawing.Size(260, 38);
             this.label4.TabIndex = 15;
-            this.label4.Text = "Ukupna suma   :";
+            this.label4.Text = "Ukupna suma  :";
             // 
             // btnDspUnpSer
             // 
-            this.btnDspUnpSer.Location = new System.Drawing.Point(418, 416);
+            this.btnDspUnpSer.Location = new System.Drawing.Point(440, 416);
             this.btnDspUnpSer.Name = "btnDspUnpSer";
             this.btnDspUnpSer.Size = new System.Drawing.Size(183, 41);
             this.btnDspUnpSer.TabIndex = 14;
@@ -136,58 +136,78 @@
             this.lvUpSer.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.hdSerNumSer,
             this.hdTypeSer,
-            this.hdDateSer,
-            this.hdPriceSer,
-            this.columnHeader1,
-            this.columnHeader2,
-            this.columnHeader3,
-            this.columnHeader4});
+            this.hdSubtypeSer,
+            this.hdFirstName,
+            this.hdSecName,
+            this.hdDtTime,
+            this.hdLicPl,
+            this.hdPrice});
             this.lvUpSer.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
             listViewItem1});
-            this.lvUpSer.Location = new System.Drawing.Point(29, 197);
+            this.lvUpSer.Location = new System.Drawing.Point(19, 191);
             this.lvUpSer.Name = "lvUpSer";
-            this.lvUpSer.Size = new System.Drawing.Size(557, 210);
+            this.lvUpSer.Size = new System.Drawing.Size(689, 210);
             this.lvUpSer.TabIndex = 13;
             this.lvUpSer.UseCompatibleStateImageBehavior = false;
             this.lvUpSer.View = System.Windows.Forms.View.Details;
             // 
             // hdSerNumSer
             // 
-            this.hdSerNumSer.Text = "Redni broj usluge";
-            this.hdSerNumSer.Width = 120;
+            this.hdSerNumSer.Text = "R.b.";
+            this.hdSerNumSer.Width = 37;
             // 
             // hdTypeSer
             // 
             this.hdTypeSer.Text = "Vrsta usluge";
-            this.hdTypeSer.Width = 137;
+            this.hdTypeSer.Width = 96;
             // 
-            // hdDateSer
+            // hdFirstName
             // 
-            this.hdDateSer.Text = "Datum usluge";
-            this.hdDateSer.Width = 137;
+            this.hdFirstName.Text = "Ime";
+            this.hdFirstName.Width = 78;
             // 
-            // hdPriceSer
+            // hdSecName
             // 
-            this.hdPriceSer.Text = "Cijena usluge";
-            this.hdPriceSer.Width = 137;
+            this.hdSecName.Text = "Prezime";
+            this.hdSecName.Width = 86;
+            // 
+            // hdDtTime
+            // 
+            this.hdDtTime.Text = "Datum usluge";
+            this.hdDtTime.Width = 94;
+            // 
+            // hdLicPl
+            // 
+            this.hdLicPl.Text = "Registarske tablice";
+            this.hdLicPl.Width = 130;
+            // 
+            // hdPrice
+            // 
+            this.hdPrice.Text = "Cijena";
+            this.hdPrice.Width = 51;
             // 
             // btnGenBill
             // 
             this.btnGenBill.Enabled = false;
-            this.btnGenBill.Location = new System.Drawing.Point(622, 367);
+            this.btnGenBill.Location = new System.Drawing.Point(629, 416);
             this.btnGenBill.Name = "btnGenBill";
-            this.btnGenBill.Size = new System.Drawing.Size(176, 40);
+            this.btnGenBill.Size = new System.Drawing.Size(169, 40);
             this.btnGenBill.TabIndex = 17;
             this.btnGenBill.Text = "Generiši račun";
             this.btnGenBill.UseVisualStyleBackColor = true;
             this.btnGenBill.Click += new System.EventHandler(this.btnGenBill_Click);
+            // 
+            // hdSubtypeSer
+            // 
+            this.hdSubtypeSer.Text = "Podvrsta usluge";
+            this.hdSubtypeSer.Width = 109;
             // 
             // uclIzdavanjeRacuna
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.btnGenBill);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblPrice);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btnDspUnpSer);
             this.Controls.Add(this.lvUpSer);
@@ -213,18 +233,18 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DateTimePicker dtpDateTo;
         private System.Windows.Forms.DateTimePicker dtpDateFrom;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblPrice;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnDspUnpSer;
         private System.Windows.Forms.ListView lvUpSer;
         private System.Windows.Forms.ColumnHeader hdSerNumSer;
         private System.Windows.Forms.ColumnHeader hdTypeSer;
-        private System.Windows.Forms.ColumnHeader hdDateSer;
-        private System.Windows.Forms.ColumnHeader hdPriceSer;
+        private System.Windows.Forms.ColumnHeader hdFirstName;
+        private System.Windows.Forms.ColumnHeader hdSecName;
         private System.Windows.Forms.Button btnGenBill;
-        private System.Windows.Forms.ColumnHeader columnHeader1;
-        private System.Windows.Forms.ColumnHeader columnHeader2;
-        private System.Windows.Forms.ColumnHeader columnHeader3;
-        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader hdDtTime;
+        private System.Windows.Forms.ColumnHeader hdLicPl;
+        private System.Windows.Forms.ColumnHeader hdPrice;
+        private System.Windows.Forms.ColumnHeader hdSubtypeSer;
     }
 }
--- Autopraonica/Autopraonica_Markus/forms/userControls/uclIzdavanjeRacuna.cs
+++ Autopraonica/Autopraonica_Markus/forms/userControls/uclIzdavanjeRacuna.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Autopraonica_Markus.Model.Entities;
 using System.Data.SqlClient;
+using System.Data.Entity.SqlServer;
+ 
+
 //**kod svih upita try catch blok
 //**ispraviti greske z u ž, s u š itd.
 namespace Autopraonica_Markus.forms.userControls
@@ -51,6 +54,7 @@ namespace Autopraonica_Markus.forms.userControls
 
         private void btnDspUnpSer_Click(object sender, EventArgs e)
         {
+            lvUpSer.Items.Clear();
             string dateFrom = dtpDateFrom.Value.ToShortDateString();
             string dateTo = dtpDateTo.Value.ToShortDateString();
             fillListOfUnpaidServices();
@@ -68,36 +72,43 @@ namespace Autopraonica_Markus.forms.userControls
             }
             if(areValidFields)
             {
+                decimal suma = 0;
+               
                 using (MarkusDb context = new MarkusDb())
                 {
-                    var listOfClientsUnpaidServices = 
-                    ( from les in context.legalentityservices
-                      join cl in context.clients on les.Client_Id equals cl.Id
-                      join nes in context.naturalentityservices on les.NaturalEntityService_Id equals nes.Id
-                      join plit in context.pricelistitems on nes.PricelistItem_Id equals plit.Id
-                      join plin in context.pricelistitemnames on plit.PricelistItemName_Id equals plin.Id
-                      join serTp in context.servicetypes on plit.ServiceType_Id equals serTp.Id
-                      where cl.Name == "EFP"
-                      select new { serTp.Name, les.FirstName,       //plin.Name,
-                          les.LastName, nes.ServiceTime , les.LicencePlate, nes.Price}).ToList();
+                  var listOfClientsUnpaidServices = 
+                  ( from les in context.legalentityservices
+                    join cl in context.clients on les.Client_Id equals cl.Id
+                    join nes in context.naturalentityservices on les.NaturalEntityService_Id equals nes.Id
+                    join plit in context.pricelistitems on nes.PricelistItem_Id equals plit.Id
+                    join plin in context.pricelistitemnames on plit.PricelistItemName_Id equals plin.Id
+                    join serTp in context.servicetypes on plit.ServiceType_Id equals serTp.Id
+                    where cl.Name == cmbClients.Text
+                  //  where  String.Compare((nes.ServiceTime.ToShortDateString()), dateFrom) >= 0 /* puca */
+                    //where String.Compare(dateTo,s =nes.ServiceTime.ToShortDateString()) >= 0  /* puca */
+                    select new { serTp.Name, les.FirstName, priceName = plin.Name ,les.LastName, nes.ServiceTime , les.LicencePlate, nes.Price}).ToList();
                     int i = 1;
                     foreach (var v in listOfClientsUnpaidServices)
                     {
                         ListViewItem item = new ListViewItem(i++.ToString());
                         item.SubItems.Add(v.Name);
+                        item.SubItems.Add(v.priceName);
                         item.SubItems.Add(v.FirstName);
                         item.SubItems.Add(v.LastName);
-                        item.SubItems.Add(v.ServiceTime.ToLongDateString());
+                        item.SubItems.Add(v.ServiceTime.ToShortDateString());
                         item.SubItems.Add(v.LicencePlate);
                         item.SubItems.Add(v.Price.ToString());
+                        suma += v.Price;
                         lvUpSer.Items.Add(item);
                     }
-                    // Console.WriteLine(listOfClientsUnpaidServices.GetType());
                 }
-
-                    btnGenBill.Enabled = true; //ovdje mora neka izmjena koda
+                lblPrice.Text = suma.ToString();
+                
+                if (lblPrice.Text == "0")
+                    btnGenBill.Enabled = false;
+                else
+                    btnGenBill.Enabled = true; 
             }
-        
         }
 
         private void fillListOfUnpaidServices() {
@@ -111,7 +122,12 @@ namespace Autopraonica_Markus.forms.userControls
 
         private void btnGenBill_Click(object sender, EventArgs e)
         {
-
+            Aspose.Pdf.Generator.Pdf pdf = new Aspose.Pdf.Generator.Pdf();
+            Aspose.Pdf.Generator.Section section1 = pdf.Sections.Add();
+            Aspose.Pdf.Generator.Text text1 = new Aspose.Pdf.Generator.Text("Hello world...");
+                section1.Paragraphs.Add(text1);
+         
+            pdf.Save("C:/Users/Nikola/Desktop/GenersianjeRacuna");
         }
     }
 }
