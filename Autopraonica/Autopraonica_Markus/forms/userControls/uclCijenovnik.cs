920482ace395e20e12998b7332190c47ba6e0580
Ognjen <Ognjen@OGNJEN-PC>
2018-12-24 03:27:14 +0100

    Popravljeni errori

--- Autopraonica/Autopraonica_Markus/forms/userControls/uclCijenovnik.cs
+++ Autopraonica/Autopraonica_Markus/forms/userControls/uclCijenovnik.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
 using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Autopraonica_Markus.forms.pricelistForms;
 using Autopraonica_Markus.Model.Entities;
 using System.Text.RegularExpressions;
+using Autopraonica_Markus.forms.pricelistForms;
 
 namespace Autopraonica_Markus.forms.userControls
 {
