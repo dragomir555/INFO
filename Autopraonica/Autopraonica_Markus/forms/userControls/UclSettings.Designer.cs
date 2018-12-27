028082e7b137dd1cf641837b9124abf1858f2e67
Dejan Mijic <mijicdejan95@gmail.com>
2018-12-27 15:03:24 +0100

    Promijenjen dizajn UclSettings.

--- Autopraonica/Autopraonica_Markus/forms/userControls/UclSettings.Designer.cs
+++ Autopraonica/Autopraonica_Markus/forms/userControls/UclSettings.Designer.cs
@@ -42,6 +42,7 @@
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.textBox7 = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblName
@@ -56,7 +57,7 @@
             // lblUID
             // 
             this.lblUID.AutoSize = true;
-            this.lblUID.Location = new System.Drawing.Point(130, 110);
+            this.lblUID.Location = new System.Drawing.Point(130, 100);
             this.lblUID.Name = "lblUID";
             this.lblUID.Size = new System.Drawing.Size(30, 16);
             this.lblUID.TabIndex = 1;
@@ -65,7 +66,7 @@
             // lblAddress
             // 
             this.lblAddress.AutoSize = true;
-            this.lblAddress.Location = new System.Drawing.Point(130, 170);
+            this.lblAddress.Location = new System.Drawing.Point(130, 150);
             this.lblAddress.Name = "lblAddress";
             this.lblAddress.Size = new System.Drawing.Size(58, 16);
             this.lblAddress.TabIndex = 2;
@@ -74,7 +75,7 @@
             // lblPhoneNumber
             // 
             this.lblPhoneNumber.AutoSize = true;
-            this.lblPhoneNumber.Location = new System.Drawing.Point(130, 230);
+            this.lblPhoneNumber.Location = new System.Drawing.Point(130, 200);
             this.lblPhoneNumber.Name = "lblPhoneNumber";
             this.lblPhoneNumber.Size = new System.Drawing.Size(96, 16);
             this.lblPhoneNumber.TabIndex = 3;
@@ -83,7 +84,7 @@
             // lblAccountNumber
             // 
             this.lblAccountNumber.AutoSize = true;
-            this.lblAccountNumber.Location = new System.Drawing.Point(130, 290);
+            this.lblAccountNumber.Location = new System.Drawing.Point(130, 250);
             this.lblAccountNumber.Name = "lblAccountNumber";
             this.lblAccountNumber.Size = new System.Drawing.Size(87, 16);
             this.lblAccountNumber.TabIndex = 4;
@@ -92,7 +93,7 @@
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Location = new System.Drawing.Point(130, 350);
+            this.lblEmail.Location = new System.Drawing.Point(130, 300);
             this.lblEmail.Name = "lblEmail";
             this.lblEmail.Size = new System.Drawing.Size(105, 16);
             this.lblEmail.TabIndex = 5;
@@ -101,7 +102,7 @@
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(130, 410);
+            this.lblPassword.Location = new System.Drawing.Point(130, 350);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(61, 16);
             this.lblPassword.TabIndex = 6;
@@ -118,7 +119,7 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(500, 100);
+            this.textBox2.Location = new System.Drawing.Point(500, 90);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(380, 26);
             this.textBox2.TabIndex = 8;
@@ -126,7 +127,7 @@
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(500, 160);
+            this.textBox3.Location = new System.Drawing.Point(500, 140);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(380, 26);
             this.textBox3.TabIndex = 9;
@@ -134,7 +135,7 @@
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(500, 220);
+            this.textBox4.Location = new System.Drawing.Point(500, 190);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(380, 26);
             this.textBox4.TabIndex = 10;
@@ -142,7 +143,7 @@
             // textBox5
             // 
             this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(500, 280);
+            this.textBox5.Location = new System.Drawing.Point(500, 240);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(380, 26);
             this.textBox5.TabIndex = 11;
@@ -150,7 +151,7 @@
             // textBox6
             // 
             this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox6.Location = new System.Drawing.Point(500, 340);
+            this.textBox6.Location = new System.Drawing.Point(500, 290);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(380, 26);
             this.textBox6.TabIndex = 12;
@@ -158,17 +159,32 @@
             // textBox7
             // 
             this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox7.Location = new System.Drawing.Point(500, 400);
+            this.textBox7.Location = new System.Drawing.Point(500, 340);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(380, 26);
             this.textBox7.TabIndex = 13;
             this.textBox7.UseSystemPasswordChar = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(65)))), ((int)(((byte)(150)))));
+            this.btnSave.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(46)))), ((int)(((byte)(140)))));
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(770, 400);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(110, 35);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Sačuvaj";
+            this.btnSave.UseVisualStyleBackColor = false;
             // 
             // UclSettings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.btnSave);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
@@ -209,5 +225,6 @@
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button btnSave;
     }
 }
