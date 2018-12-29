a419698c7c527df32220940c2a937074a1deebd3
Ognjen <Ognjen@OGNJEN-PC>
2018-12-29 02:49:05 +0100

    Popravljena uclStatistika. Dodano pretrazivanje po imenu ili nazivu.

--- Autopraonica/Autopraonica_Markus/forms/userControls/uclStatistika.Designer.cs
+++ Autopraonica/Autopraonica_Markus/forms/userControls/uclStatistika.Designer.cs
@@ -30,138 +30,178 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.cbStatistics = new System.Windows.Forms.ComboBox();
-            this.cbEmployee = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.cbClient = new System.Windows.Forms.ComboBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.btnGenerateStatistics = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.splitter2 = new System.Windows.Forms.Splitter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 29);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(255, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.Size = new System.Drawing.Size(154, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Odaberite vrstu statistike";
             // 
             // cbStatistics
             // 
+            this.cbStatistics.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.cbStatistics.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbStatistics.FormattingEnabled = true;
             this.cbStatistics.Items.AddRange(new object[] {
             "Statistika po radniku",
             "Statistika po klijentu",
             "Ukupna statistika"});
-            this.cbStatistics.Location = new System.Drawing.Point(191, 26);
+            this.cbStatistics.Location = new System.Drawing.Point(415, 29);
             this.cbStatistics.Name = "cbStatistics";
-            this.cbStatistics.Size = new System.Drawing.Size(121, 21);
+            this.cbStatistics.Size = new System.Drawing.Size(161, 21);
             this.cbStatistics.TabIndex = 1;
             this.cbStatistics.SelectedIndexChanged += new System.EventHandler(this.cbStatistics_SelectedIndexChanged);
             // 
-            // cbEmployee
-            // 
-            this.cbEmployee.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbEmployee.Enabled = false;
-            this.cbEmployee.FormattingEnabled = true;
-            this.cbEmployee.Location = new System.Drawing.Point(36, 144);
-            this.cbEmployee.Name = "cbEmployee";
-            this.cbEmployee.Size = new System.Drawing.Size(121, 21);
-            this.cbEmployee.TabIndex = 2;
-            this.cbEmployee.Format += new System.Windows.Forms.ListControlConvertEventHandler(this.cbFormat);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(49, 128);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Odaberite radnika";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(233, 128);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Unesite klijenta";
-            // 
-            // cbClient
-            // 
-            this.cbClient.Enabled = false;
-            this.cbClient.FormattingEnabled = true;
-            this.cbClient.Location = new System.Drawing.Point(216, 144);
-            this.cbClient.Name = "cbClient";
-            this.cbClient.Size = new System.Drawing.Size(121, 21);
-            this.cbClient.TabIndex = 5;
-            this.cbClient.SelectedIndexChanged += new System.EventHandler(this.cbClient_SelectedIndexChanged);
-            // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(66, 231);
+            this.dateTimePicker1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(209, 413);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(214, 20);
             this.dateTimePicker1.TabIndex = 6;
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(66, 268);
+            this.dateTimePicker2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(209, 452);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.Size = new System.Drawing.Size(214, 20);
             this.dateTimePicker2.TabIndex = 7;
             // 
             // label4
             // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(27, 237);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(168, 417);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.Size = new System.Drawing.Size(26, 16);
             this.label4.TabIndex = 8;
             this.label4.Text = "Od";
             // 
             // label5
             // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(27, 274);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(168, 456);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.Size = new System.Drawing.Size(26, 16);
             this.label5.TabIndex = 9;
             this.label5.Text = "Do";
             // 
             // btnGenerateStatistics
             // 
-            this.btnGenerateStatistics.Location = new System.Drawing.Point(259, 340);
+            this.btnGenerateStatistics.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnGenerateStatistics.Enabled = false;
+            this.btnGenerateStatistics.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerateStatistics.Location = new System.Drawing.Point(248, 487);
             this.btnGenerateStatistics.Name = "btnGenerateStatistics";
-            this.btnGenerateStatistics.Size = new System.Drawing.Size(98, 23);
+            this.btnGenerateStatistics.Size = new System.Drawing.Size(132, 34);
             this.btnGenerateStatistics.TabIndex = 10;
-            this.btnGenerateStatistics.Text = "Generisi statistiku";
+            this.btnGenerateStatistics.Text = "Generiši statistiku";
             this.btnGenerateStatistics.UseVisualStyleBackColor = true;
             this.btnGenerateStatistics.Click += new System.EventHandler(this.btnGenerateStatistics_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(139, 383);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(356, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Odaberite za koji period želite pregledati statistiku";
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 524);
+            this.splitter1.TabIndex = 13;
+            this.splitter1.TabStop = false;
+            // 
+            // splitter2
+            // 
+            this.splitter2.Location = new System.Drawing.Point(3, 0);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(3, 524);
+            this.splitter2.TabIndex = 14;
+            this.splitter2.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Enabled = false;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(546, 234);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSearch.Enabled = false;
+            this.tbSearch.Location = new System.Drawing.Point(371, 300);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(205, 20);
+            this.tbSearch.TabIndex = 15;
+            this.tbSearch.TextChanged += new System.EventHandler(this.tbSearch_TextChanged);
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Enabled = false;
+            this.lblSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearch.Location = new System.Drawing.Point(245, 301);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(111, 16);
+            this.lblSearch.TabIndex = 16;
+            this.lblSearch.Text = "Pretraži po imenu";
             // 
             // uclStatistika
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.tbSearch);
+            this.Controls.Add(this.splitter2);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnGenerateStatistics);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.dateTimePicker2);
             this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.cbClient);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.cbEmployee);
             this.Controls.Add(this.cbStatistics);
             this.Controls.Add(this.label1);
             this.Name = "uclStatistika";
-            this.Size = new System.Drawing.Size(601, 382);
+            this.Size = new System.Drawing.Size(601, 524);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,14 +211,16 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cbStatistics;
-        private System.Windows.Forms.ComboBox cbEmployee;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox cbClient;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.DateTimePicker dateTimePicker2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button btnGenerateStatistics;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Splitter splitter2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.Label lblSearch;
     }
 }
--- Autopraonica/Autopraonica_Markus/forms/userControls/uclStatistika.cs
+++ Autopraonica/Autopraonica_Markus/forms/userControls/uclStatistika.cs
@@ -34,44 +34,104 @@ namespace Autopraonica_Markus.forms.userControls
 
         private void cbStatistics_SelectedIndexChanged(object sender, EventArgs e)
         {
+            btnGenerateStatistics.Enabled = true;
             Object selectedItem = cbStatistics.SelectedItem;
             if("Statistika po radniku".Equals(selectedItem.ToString()))
             {
-                cbEmployee.Enabled = true;
-                cbClient.Enabled = false;
-                fillEmployeeComboBox();
+                dataGridView1.Enabled = true;
+                lblSearch.Enabled = true;
+                tbSearch.Enabled = true;
+                lblSearch.Text = "Pretraži po imenu";
+                fillEmployeeDataGrid();
             } else if("Statistika po klijentu".Equals(selectedItem.ToString()))
             {
-
-                cbEmployee.Enabled = false;
-                cbClient.Enabled = true;
-                fillClientComboBox();
+                dataGridView1.Enabled = true;
+                lblSearch.Enabled = true;
+                tbSearch.Enabled = true;
+                lblSearch.Text = "Pretraži po nazivu";
+                fillClientDataGrid();
             } else
             {
-                cbEmployee.Enabled = false;
-                cbClient.Enabled = false;
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                dataGridView1.ColumnCount = 0;
+                dataGridView1.Enabled = false;
+                lblSearch.Enabled = false;
+                tbSearch.Enabled = false;
             }
         }
 
-        private void fillEmployeeComboBox()
+
+        private void fillEmployeeDataGrid()
         {
+
+            string text = tbSearch.Text.Trim();
+
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].Name = "Ime";
+            dataGridView1.Columns[1].Name = "Prezime";
+            dataGridView1.Columns[2].Name = "Id";
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[2].Visible = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
             using (MarkusDb context = new MarkusDb())
             {
-                var employees = context.employees.ToList();
-                cbEmployee.DataSource = employees;
-                cbEmployee.ValueMember = "Id";
-                cbEmployee.DisplayMember = "FirstName";
+                var employees = (
+                    from e in context.employees
+                    where e.FirstName.StartsWith(text)
+                    select e
+                    );
+                foreach(var e in employees)
+                {
+                    DataGridViewRow row = new DataGridViewRow()
+                    {
+                        Tag = e
+                    };
+                    row.CreateCells(dataGridView1);
+                    row.SetValues(e.FirstName, e.LastName, e.Id);
+                    dataGridView1.Rows.Add(row);
+                }
             }
         }
 
-        private void fillClientComboBox()
+        private void fillClientDataGrid()
         {
+            string text = tbSearch.Text.Trim();
+
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.ColumnCount = 2;
+            dataGridView1.Columns[0].Name = "Naziv";
+            dataGridView1.Columns[1].Name = "Id";
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[1].Visible = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
             using (MarkusDb context = new MarkusDb())
             {
-                var clients = context.clients.ToList();
-                cbClient.DataSource = clients;
-                cbClient.ValueMember = "Id";
-                cbClient.DisplayMember = "Name";
+                var clients = (
+                    from c in context.clients
+                    where c.Name.StartsWith(text)
+                    select c
+                    );
+                foreach(var c in clients)
+                {
+                    DataGridViewRow row = new DataGridViewRow()
+                    {
+                        Tag = c
+                    };
+                    row.CreateCells(dataGridView1);
+                    row.SetValues(c.Name, c.Id);
+                    dataGridView1.Rows.Add(row);
+                }
             }
         }
 
@@ -82,10 +142,25 @@ namespace Autopraonica_Markus.forms.userControls
             e.Value = lastname + " " + firstname;
         }
 
-        private void cbClient_SelectedIndexChanged(object sender, EventArgs e)
+    /*    private void textBox1_KeyUp(object sender, KeyEventArgs e)
         {
-
-        }
+            
+            foreach (System.Windows.Forms.DataGridViewRow r in dataGridView1.Rows)
+            {
+                if ((r.Cells[0].Value).ToString().ToUpper().Contains(textBox1.Text.ToUpper()))
+                {
+                    dataGridView1.Rows[r.Index].Visible = true;
+                    dataGridView1.Rows[r.Index].Selected = true;
+                }
+                else
+                {
+                    dataGridView1.CurrentCell = null;
+                    dataGridView1.Rows[r.Index].Visible = false;
+                }
+            }
+            Console.WriteLine("Dobar dan " + textBox1.Text);
+            
+        }*/
 
         private void btnGenerateStatistics_Click(object sender, EventArgs e)
         {
@@ -93,18 +168,32 @@ namespace Autopraonica_Markus.forms.userControls
             DateTime dt2 = this.dateTimePicker2.Value.Date;
             if ("Statistika po radniku".Equals(cbStatistics.Text))
             {
-                int id = (int)cbEmployee.SelectedValue;
-                EmployeeChartForm ecf = new EmployeeChartForm(id, cbEmployee.Text, dt1, dt2);
+                int id = (int)dataGridView1.SelectedRows[0].Cells[2].Value;
+                string name = dataGridView1.SelectedRows[0].Cells[0].Value.ToString() + dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+                EmployeeChartForm ecf = new EmployeeChartForm(id, name, dt1, dt2);
                 ecf.ShowDialog();
             } else if("Statistika po klijentu".Equals(cbStatistics.Text))
             {
-                int id = (int)cbClient.SelectedValue;
-                ClientChartForm ccf = new ClientChartForm(id, cbClient.Text, dt1, dt2);
+                int id = (int)dataGridView1.SelectedRows[0].Cells[1].Value;
+                string name = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+                ClientChartForm ccf = new ClientChartForm(id, name, dt1, dt2);
                 ccf.ShowDialog();
             } else
             {
                 TotalStatisticsChartForm tscf = new TotalStatisticsChartForm(dt1, dt2);
                 tscf.ShowDialog();
+            }
+        }
+
+
+        private void tbSearch_TextChanged(object sender, EventArgs e)
+        {
+            if("Statistika po radniku".Equals(cbStatistics.SelectedItem.ToString()))
+            {
+                fillEmployeeDataGrid();
+            } else
+            {
+                fillClientDataGrid();
             }
         }
     }
