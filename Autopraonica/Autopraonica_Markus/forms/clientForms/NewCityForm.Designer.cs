91438e08616fbf730b25019683a83603b7923ede
Dejan Mijic <mijicdejan95@gmail.com>
2019-02-28 15:22:06 +0100

    Promjena boje forme za dodavanje grada.

--- Autopraonica/Autopraonica_Markus/forms/clientForms/NewCityForm.Designer.cs
+++ Autopraonica/Autopraonica_Markus/forms/clientForms/NewCityForm.Designer.cs
@@ -45,10 +45,9 @@
             this.lblCityName.AutoSize = true;
             this.lblCityName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.lblCityName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(46)))), ((int)(((byte)(140)))));
-            this.lblCityName.Location = new System.Drawing.Point(52, 53);
-            this.lblCityName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCityName.Location = new System.Drawing.Point(39, 43);
             this.lblCityName.Name = "lblCityName";
-            this.lblCityName.Size = new System.Drawing.Size(109, 20);
+            this.lblCityName.Size = new System.Drawing.Size(92, 16);
             this.lblCityName.TabIndex = 0;
             this.lblCityName.Text = "Naziv grada";
             // 
@@ -57,30 +56,27 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(46)))), ((int)(((byte)(140)))));
-            this.label2.Location = new System.Drawing.Point(56, 113);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(42, 92);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(129, 20);
+            this.label2.Size = new System.Drawing.Size(107, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Poštanski broj";
             // 
             // tbCityName
             // 
             this.tbCityName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.tbCityName.Location = new System.Drawing.Point(239, 53);
-            this.tbCityName.Margin = new System.Windows.Forms.Padding(4);
+            this.tbCityName.Location = new System.Drawing.Point(179, 43);
             this.tbCityName.Name = "tbCityName";
-            this.tbCityName.Size = new System.Drawing.Size(285, 30);
+            this.tbCityName.Size = new System.Drawing.Size(215, 26);
             this.tbCityName.TabIndex = 1;
             this.tbCityName.Validating += new System.ComponentModel.CancelEventHandler(this.tbCityName_Validating);
             // 
             // tbPostCode
             // 
             this.tbPostCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.tbPostCode.Location = new System.Drawing.Point(239, 103);
-            this.tbPostCode.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPostCode.Location = new System.Drawing.Point(179, 84);
             this.tbPostCode.Name = "tbPostCode";
-            this.tbPostCode.Size = new System.Drawing.Size(285, 30);
+            this.tbPostCode.Size = new System.Drawing.Size(215, 26);
             this.tbPostCode.TabIndex = 2;
             this.tbPostCode.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbPostCode_KeyPress);
             this.tbPostCode.Validating += new System.ComponentModel.CancelEventHandler(this.tbPostCode_Validating);
@@ -93,10 +89,9 @@
             this.btnConfirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.btnConfirm.ForeColor = System.Drawing.Color.White;
-            this.btnConfirm.Location = new System.Drawing.Point(60, 196);
-            this.btnConfirm.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConfirm.Location = new System.Drawing.Point(45, 159);
             this.btnConfirm.Name = "btnConfirm";
-            this.btnConfirm.Size = new System.Drawing.Size(160, 43);
+            this.btnConfirm.Size = new System.Drawing.Size(120, 35);
             this.btnConfirm.TabIndex = 4;
             this.btnConfirm.Text = "Potvrdi";
             this.btnConfirm.UseVisualStyleBackColor = false;
@@ -110,10 +105,9 @@
             this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.btnCancel.ForeColor = System.Drawing.Color.White;
-            this.btnCancel.Location = new System.Drawing.Point(365, 196);
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancel.Location = new System.Drawing.Point(274, 159);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(160, 43);
+            this.btnCancel.Size = new System.Drawing.Size(120, 35);
             this.btnCancel.TabIndex = 5;
             this.btnCancel.Text = "Otkaži";
             this.btnCancel.UseVisualStyleBackColor = false;
@@ -126,10 +120,11 @@
             // 
             // NewCityForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
-            this.ClientSize = new System.Drawing.Size(591, 267);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(443, 217);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnConfirm);
             this.Controls.Add(this.tbPostCode);
@@ -138,7 +133,6 @@
             this.Controls.Add(this.lblCityName);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "NewCityForm";
